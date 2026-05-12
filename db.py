e74021a151d7699fd036f77613128c32c3b5c83c
formtpz <137564298+formtpz@users.noreply.github.com>
2026-05-12 15:09:22 -0600

    Update db.py

--- db.py
+++ db.py
@@ -1,17 +1,20 @@
 # db.py
 import psycopg2
 import streamlit as st
+from sqlalchemy import create_engine
+from urllib.parse import urlparse
 
 @st.cache_resource
 def get_connection():
     return psycopg2.connect(st.secrets["db_credentials"]["URI"])
 
+@st.cache_resource
+def get_engine():
+    """Crea un engine de SQLAlchemy para pandas.read_sql"""
+    uri = st.secrets["db_credentials"]["URI"]
+    return create_engine(uri)
 
 def execute(query: str, params=None):
-    """
-    Ejecuta una consulta SQL (INSERT, UPDATE, DELETE) con parámetros.
-    Similar a la función execute() que existía en db_core.py
-    """
     conn = get_connection()
     cur = conn.cursor()
     try:
